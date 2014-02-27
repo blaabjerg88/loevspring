ee0d8bd722902ca46468794dc7ec267de2b0b6c7
Oscar Diös <okdios@gmail.com>
2014-02-27 14:25:42 +0100

    Added login functionality, and som eminor changes/fixes.

--- res/js/webservice.js
+++ res/js/webservice.js
@@ -1,15 +1,20 @@
+/*********************************************************************
+Add this where you want to implement a call to the webservice:
+
 $("#testButton").click(function(){
-	callWebservice('method=getComments&image_id=1', 'onCallback');
+    callWebservice('method=getComments&image_id=1', 'YOUR_CALLBACK_FUNCTION');
 })
+function YOUR_CALLBACK_FUNCTION (data) {
+    console.log("onCallback")
+    console.log(data)
+}
+*********************************************************************/
+
 function callWebservice (params, callbackFunc) {
-	var script = document.createElement('script');
+    var script = document.createElement('script');
     script.type = "text/javascript";
-    script.src = "http://markblaabjerg.dyndns.org/test_webservice/index.php?callback="+ callbackFunc + "&" + params;
+    script.src = "http://markblaabjerg.dyndns.org/webservice/index.php?callback="+ callbackFunc + "&" + params;
     var head = document.head;
     head.appendChild(script);
     head.removeChild(script);
-}
-function onCallback (data) {
-    console.log("onCallback")
-    console.log(data)
 }
