cf61c8d5d717fae60d12df85010566c7f40fb39c
Oscar Diös <okdios@gmail.com>
2014-02-27 15:47:36 +0100

    Fixed navigation problem. And minor changes/fixes..

--- res/js/custom.js
+++ res/js/custom.js
@@ -10,6 +10,17 @@ function YOUR_CALLBACK_FUNCTION (data) {
 }
 *********************************************************************/
 
+/*
+$("#contentMenu a").click(function(e){
+    console.log("ELEM: " + e);
+    return false;
+})*/
+
+function navigate(page)
+{
+    window.location.href = page;
+}
+
 function callWebservice (params, callbackFunc) {
     var script = document.createElement('script');
     script.type = "text/javascript";
@@ -18,3 +29,4 @@ function callWebservice (params, callbackFunc) {
     head.appendChild(script);
     head.removeChild(script);
 }
+
