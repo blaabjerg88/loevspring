ffdf2bf4504bdcf871f11765ca1fa6bbfbfc9ede
alped200 <alex.lethp@gmail.com>
2014-03-03 13:42:49 +0100

    Finished fullscreen, changes in .css

---
+++ res/js/webservice.js
@@ -0,0 +1,15 @@
+$("#testButton").click(function(){
+	callWebservice('method=getComments&image_id=1', 'onCallback');
+})
+function callWebservice (params, callbackFunc) {
+	var script = document.createElement('script');
+    script.type = "text/javascript";
+    script.src = "http://markblaabjerg.dyndns.org/webservice/index.php?callback="+ callbackFunc + "&" + params;
+    var head = document.head;
+    head.appendChild(script);
+    head.removeChild(script);
+}
+function onCallback (data) {
+    console.log("onCallback")
+    console.log(data)
+}
